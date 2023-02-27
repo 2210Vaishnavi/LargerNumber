8374866687b6325de9997b8f8beae77530940f1c
kajalkhirodkar <vaishnavikulkarni2210@gmail.com>
2023-02-27 14:47:54 +0530

    committing Larger Number .java file

---
+++ LargerNumber.java
@@ -0,0 +1,24 @@
+package com.acts;
+
+import java.util.Scanner;
+
+public class LargerNumber {
+
+	public static void main(String[] args) {
+
+
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Two Numbers");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		if(num1> num2){
+			System.out.println("Larger Number is: " + num1);
+		}
+		else{
+			System.out.println("Larger Number is: " + num2);
+		}
+		sc.close();
+	}
+
+}
